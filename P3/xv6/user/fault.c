e209b3d8e5ada18460f988c2bae1b910bdf9fb98
Emanuel Rosu <rosu.emanuel@gmail.com>
2015-07-22 23:12:54 -0700

    removing xv6 projects for readability

--- P3/xv6/user/fault.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-#include "user.h"
-
-int
-main(int argc, char *argv[]){
-  int *p = 0;// p is on a stack
-  printf(1, "%x\n", *p);// try to read the value at address 0;
-  exit();
-}
